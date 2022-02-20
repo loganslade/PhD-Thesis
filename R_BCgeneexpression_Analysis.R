c9e411773b0514ab2a9abf1a77c5aaccd2f7c246
loganslade <lgnslade@gmail.com>
2022-02-20 00:19:58 -0400

    Update R_BCgeneexpression_Analysis.R
    Fix errors, changes graphs to green

--- R_BCgeneexpression_Analysis.R
+++ R_BCgeneexpression_Analysis.R
@@ -95,7 +95,7 @@ bc.surv$Subtype <- recode_factor(bc.surv$Subtype, LumA = "Luminal A",
                                                       LumB = "Luminal B", 
                                                       Normal = "Normal-like", 
                                                       Her2 = "HER2-enriched", 
-                                                      Basal = "Basal-Like", 
+                                                      Basal = "Basal-Like")
 
 bc.surv$pam_tnbc <- factor(bc.surv$pam_tnbc, levels = c("LumA", "LumB", "Normal_like", "Her2", 
                                                             "BL1", "BL2", "IM", "LAR", "M", "MSL", "UNS"))
@@ -123,8 +123,8 @@ l <- as.numeric(q[2])
 m <- as.numeric(q[3])
 h <- as.numeric(q[4])
 
-bc.km <- bc.tsurv %>% mutate(strata = ifelse(TFEB  < l, "LOW", 
-                                                ifelse(between(bc.tsurv$TFEB, l, h), "MEDIAN",
+bc.km <- bc.surv %>% mutate(strata = ifelse(TFEB  < l, "LOW", 
+                                                ifelse(between(bc.surv$TFEB, l, h), "MEDIAN",
                                                         "HIGH")))
 
 
@@ -137,33 +137,73 @@ k + labs(title = "METABRIC", x= "Time (Months)", y = "Overall Survival (%)",
   scale_y_continuous(limits = c(0.3,1), labels = scales::percent)+
   scale_color_manual(values = c("#619CFF", "#00BA38", "#F8766D"))+
   theme_classic()+
-  theme(text = element_text(size=16),
-        axis.text = element_text(colour = "black", size = 14),
-        plot.title = element_text(hjust = 0.5),)
-
+  theme(text = element_text(size=18),
+        axis.text = element_text(colour = "black", size = 20),
+        plot.title = element_text(hjust = 0.5),
+        legend.position = c(0.8,.7),
+        plot.margin = margin(0,1,0,0, "cm"))
+ggsave("KM_tfeb_metabric_3s.pdf", device = "pdf", width = 14.94, height = 13.71, units = "cm")
 ###################
 #########Univariate plots########
 a <- bc.r %>% drop_na(Subtype)%>% filter(Subtype != "claudin-low") %>% ggplot(aes(Subtype, TFEB)) 
-a + geom_jitter(alpha = 0.1, colour = "red3", size = 0.75, width = 0.2)+
+a + geom_jitter(alpha = 0.2, colour = "green4", size = 1, width = 0.2)+
   geom_boxplot(fill = "grey65", colour = "black",  
                outlier.shape = NA, alpha=0.75, 
                fatten = 1, lwd = 0.5, width = 0.5, notch = TRUE)+
   ylim(5.15,6.3)+
   theme_classic()+
   labs(title = "", y=expression('TFEB Log'[2]*' Intensity'), x="")+
-  theme(text = element_text(size=14),
+  theme(text = element_text(size=18),
         plot.title = element_text(hjust = 0.5),
         axis.text = element_text(colour = "black"),
         axis.text.x = element_text(angle = 45, hjust = 1),
         panel.background = element_rect(fill = "white"))
 
-ggsave("box_tfeb_metabric_pam50_v3.pdf", device = "pdf", width = 4.88, height = 6.26, units = "in")
+ggsave("box_tfeb_metabric_pam50_v4.pdf", device = "pdf", width = 5.16, height = 6.61, units = "in")
+
+b <- bc.r %>% drop_na(three.gene)%>% ggplot(aes(three.gene, TFEB)) 
+b + geom_jitter(alpha = 0.2, colour = "green4", size = 1, width = 0.2)+
+  geom_boxplot(fill = "grey65", colour = "black",  
+               outlier.shape = NA, alpha=0.75, 
+               fatten = 1, lwd = 0.5, width = 0.5, notch = TRUE)+
+  ylim(5.15,6.3)+
+  theme_classic()+
+  labs(title = "", y=expression('TFEB Log'[2]*' Intensity'), x="")+
+  theme(text = element_text(size=18),
+        plot.title = element_text(hjust = 0.5),
+        axis.text = element_text(colour = "black"),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.background = element_rect(fill = "white"),
+        plot.margin = margin(0,0,0,1, "cm"))
+
+ggsave("box_tfeb_metabric_3gene_v4.pdf", device = "pdf", width = 4.38, height = 5.61, units = "in")
+
+c <- bc.r %>% drop_na(ihc_er)%>% ggplot(aes(ihc_er, TFEB)) 
+c + geom_jitter(alpha = 0.2, colour = "green4", size = 1, width = 0.2)+
+  geom_boxplot(fill = "grey65", colour = "black",  
+               outlier.shape = NA, alpha=0.75, 
+               fatten = 1, lwd = 0.5, width = 0.5, notch = TRUE)+
+  ylim(5.15,6.3)+
+  theme_classic()+
+  labs(title = "", y=expression('TFEB Log'[2]*' Intensity'), x="")+
+  theme(text = element_text(size=18),
+        plot.title = element_text(hjust = 0.5),
+        axis.text = element_text(colour = "black"),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.background = element_rect(fill = "white"),
+        plot.margin = margin(0,0,0,0, "cm"))
+
+ggsave("box_tfeb_metabric_ihc_er_v4.pdf", device = "pdf", width = 3.77, height = 4.83, units = "in")
+
 
 fit <- aov(TFEB ~ ihc_er, data = bc.clin)
 summary(fit)
 resaov <- as.data.frame(TukeyHSD(fit)[1]) %>% write.csv("res_TFEB_ihc_er_v3.csv")
 
 ######TCGA Data########
+dir <- "C:/Users/lslad/Desktop/Research/Research Project/Cancer projects/Bioinformatics/METABRIC-expressiondata/TCGA/TCGA Broad RSEM Norm"
+setwd(dir)
+
 tcgageneex <- read_csv("TCGA_brca_rnaseq_rsem.csv")
 tcgasample <- read_csv("TCGA_ids_clin.csv")
 ids.pam50 <- read_csv("nature_tcga_id_isubtypes.csv")
@@ -196,7 +236,7 @@ tcgabc.c <- tcgabc.u
 tcgabc.c$sample <- sample.id.2
 tcgabc.c <- tcgabc.c[!duplicated(tcgabc.c$sample),]
 
-tcgabc.clin <- dplyr::inner_join(x=tcgabc.c, y=sample.2, by = c("sample" = "patient_id"))
+tcgabc.clin <- dplyr::inner_join(x=tcgabc.c, y=tcgasample.2, by = c("sample" = "patient_id"))
 tcgabc.clin <- tcgabc.clin[!duplicated(tcgabc.clin$sample),]
 tcgabc.clin$ihc_st <- factor(tcgabc.clin$ihc_st, levels = c("ER+/HER2-", "ER+/HER2","ER+/HER2+","HER2", "HER2+", "ER-/HER2-"))
 tcgabc.clin$ihc_er <- factor(tcgabc.clin$ihc_er, levels = c("Positive","Negative"))
@@ -208,21 +248,53 @@ tcgabc.pam <- drop_na(tcgabc.pam, subtype)
 tcgabc.pam$subtype <- factor(tcgabc.pam$subtype, levels = c("Luminal A","Luminal B","Normal-like","HER2-enriched","Basal-like"))
 
 #####BOX-plots#######
-a <- tcgabc.clin %>% drop_na(ihc_er) %>% ggplot(aes(ihc_er, TFEB)) 
-a + geom_jitter(alpha = 0.1, colour = "red3", size = 0.75, width = 0.2)+
+a <- tcgabc.pam %>% drop_na(subtype) %>% ggplot(aes(subtype, TFEB)) 
+a + geom_jitter(alpha = 0.2, colour = "green4", size = 1, width = 0.2)+
   geom_boxplot(fill = "grey65", colour = "black",  
                outlier.shape = NA, alpha=0.75, 
                fatten = 1, lwd = 0.5, width = 0.5, notch = TRUE)+
-  ylim(0,2500)+
+  ylim(0,1600)+
   theme_classic()+
   labs(title = "", y="TFEB Normalized Expression", x="")+
-  theme(text = element_text(size=14),
+  theme(text = element_text(size=18),
         plot.title = element_text(hjust = 0.5),
         axis.text = element_text(colour = "black"),
         axis.text.x = element_text(angle = 45, hjust = 1),
         panel.background = element_rect(fill = "white"))
 
-ggsave("box_TFEB_tcga_ihcer_v4.pdf", device = "pdf", width = 4.88, height = 6.26, units = "in")
+ggsave("box_tfeb_tcga_pam50_v5.pdf", device = "pdf", width = 5.16, height = 6.61, units = "in")
+
+b <- tcgabc.clin %>% drop_na(ihc_st) %>% ggplot(aes(ihc_st, TFEB)) 
+b + geom_jitter(alpha = 0.2, colour = "green4", size = 1, width = 0.2)+
+  geom_boxplot(fill = "grey65", colour = "black",  
+               outlier.shape = NA, alpha=0.75, 
+               fatten = 1, lwd = 0.5, width = 0.5, notch = TRUE)+
+  ylim(0,1600)+
+  theme_classic()+
+  labs(title = "", y="TFEB Normalized Expression", x="")+
+  theme(text = element_text(size=18),
+        plot.title = element_text(hjust = 0.5),
+        axis.text = element_text(colour = "black"),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.background = element_rect(fill = "white"))
+
+ggsave("box_tfeb_tcga_ihc_v5.pdf", device = "pdf", width = 4.15, height = 5.32, units = "in")
+
+c <- tcgabc.clin %>% drop_na(ihc_er) %>% ggplot(aes(ihc_er, MAP4K3)) 
+c + geom_jitter(alpha = 0.2, colour = "green4", size = 1, width = 0.2)+
+  geom_boxplot(fill = "grey65", colour = "black",  
+               outlier.shape = NA, alpha=0.75, 
+               fatten = 1, lwd = 0.5, width = 0.5, notch = TRUE)+
+  ylim(0,2500)+
+  theme_classic()+
+  labs(title = "", y="MAP4K3 Normalized Expression", x="")+
+  theme(text = element_text(size=18),
+        plot.title = element_text(hjust = 0.5),
+        axis.text = element_text(colour = "black"),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.background = element_rect(fill = "white"))
+
+ggsave("box_MAP4K3_tcga_ihcer_v5.pdf", device = "pdf", width = 4.5, height = 5.77, units = "in")
 
 ####Stats####
 fit <- aov(TFEB ~ ihc_er, data = tcgabc.clin)
